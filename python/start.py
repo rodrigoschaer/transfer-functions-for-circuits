b23d262adcbfb3eb3fbc82e38509444cddadf087
Rodrigo F. Schaer <rodrigo.fscs@gmail.com>
2024-05-14 21:45:59 -0300

    modifies start.py

--- python/start.py
+++ python/start.py
@@ -1,8 +1,15 @@
-from sympy import init_printing, factor, expand, symbols
+from sympy import Eq, symbols, init_printing, solve
+from sympy.abc import s
 init_printing(use_unicode=True)
 
-x, y = symbols('x y')
-expr = x + 2*y
 
-expanded_expr = expand(x*expr)
-print(factor(expanded_expr))
+R1, R2, C1, C2 = symbols('R1 R2 C1 C2', positive=True)
+V1, V2, I1, I2, Vout = symbols('V1 V2 I1 I2 Vout')
+
+eq1 = Eq(s*(C1+C2)*V1 + (1/R1 + s*C1)*I1 - s*C2*V2, 0)
+eq2 = Eq(-s*C2*V1 + (1/R2 + s*C2)*I2 + V2/(R2*s), 0)
+eq3 = Eq(Vout/R2, I2)
+
+sol = solve([eq1, eq2, eq3], [Vout, I1])
+
+print(sol)
