2df8133ff397f9a07caaf950a5e806397622bf55
Rodrigo F. Schaer <rodrigo.fscs@gmail.com>
2024-08-04 00:40:09 -0300

    feat: adds concise netlist

--- python/assets/netlist.py
+++ python/assets/netlist.py
@@ -1,8 +1,8 @@
-NETLIST = """
-* Example Circuit
-Vs V1 0 DC 0
-RB V1 V2 RB
-r_pi V2 V3 r_pi
-I1 V3 0 DC gm*V_be
-RC V3 V0 RC
+COMMON_EMITTER = """
+R_PI V_IN V_E R_PI
+I2 V_OUT V_E gm*(V_IN-V_E)
+R_C V_OUT 0 R_C
+C_PI V_IN V_E C_PI
+R_E V_E 0 R_E
+I1 0 V_IN I_IN
 """
