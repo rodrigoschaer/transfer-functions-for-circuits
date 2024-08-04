5d7c24215d76a2cbaf16ccfe7a87e1efa7c1562d
Rodrigo F. Schaer <rodrigo.fscs@gmail.com>
2024-08-04 00:41:16 -0300

    fix: fixes KCL methods to calculate components contributions

--- python/steps/write_kcl_equations.py
+++ python/steps/write_kcl_equations.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 import sympy as sp
 
 
@@ -14,65 +13,55 @@ def current_through_inductor(v1, v2, inductance, s):
     return (v1 - v2) / (s * inductance)
 
 
-def write_kcl_equations(node_connections, node_voltages, s, symbols):
-    kcl_equations = defaultdict()
+def write_kcl_equations(node_connections, node_voltages, symbols):
+    s = sp.symbols("s")
+    kcl_equations = {}
     for node, connections in node_connections.items():
-        if node == "0":  # Skip ground node
+        if node == "0":
             continue
 
         node_current = 0
-        for name, comp_nodes, value in connections:
-            if name.startswith("R"):
+        for comp_name, comp_nodes, _ in connections:
+            if comp_name.startswith("R"):
                 # Resistor
-                if comp_nodes[0] == node:
-                    node_current += current_through_resistor(
-                        node_voltages[node], node_voltages[comp_nodes[1]], symbols[name]
-                    )
-                else:
-                    node_current += current_through_resistor(
-                        node_voltages[comp_nodes[1]], node_voltages[node], symbols[name]
-                    )
-            elif name.startswith("C"):
+                contribution = current_through_resistor(
+                    node_voltages[comp_nodes[0]],
+                    node_voltages[comp_nodes[1]],
+                    symbols[comp_name],
+                )
+                node_current += contribution if comp_nodes[0] == node else -contribution
+
+            elif comp_name.startswith("C"):
                 # Capacitor
-                if comp_nodes[0] == node:
-                    node_current += current_through_capacitor(
-                        node_voltages[node],
-                        node_voltages[comp_nodes[1]],
-                        symbols[name],
-                        s,
-                    )
-                else:
-                    node_current += current_through_capacitor(
-                        node_voltages[comp_nodes[1]],
-                        node_voltages[node],
-                        symbols[name],
-                        s,
-                    )
-            elif name.startswith("L"):
+                contribution = current_through_capacitor(
+                    node_voltages[comp_nodes[0]],
+                    node_voltages[comp_nodes[1]],
+                    symbols[comp_name],
+                    s,
+                )
+                node_current += contribution
+            elif comp_name.startswith("L"):
                 # Inductor
-                if comp_nodes[0] == node:
-                    node_current += current_through_inductor(
-                        node_voltages[node],
-                        node_voltages[comp_nodes[1]],
-                        symbols[name],
-                        s,
-                    )
-                else:
-                    node_current += current_through_inductor(
-                        node_voltages[comp_nodes[1]],
-                        node_voltages[node],
-                        symbols[name],
-                        s,
-                    )
-            elif name.startswith("V"):
+                contribution = current_through_inductor(
+                    node_voltages[comp_nodes[0]],
+                    node_voltages[comp_nodes[1]],
+                    symbols[comp_name],
+                    s,
+                )
+                node_current += contribution if comp_nodes[0] == node else -contribution
+            elif comp_name.startswith("V"):
                 # Independent voltage source
                 pass
-            elif name.startswith("I"):
+            elif comp_name.startswith("I"):
                 # Independent current source
                 if comp_nodes[0] == node:
-                    node_current += symbols[name]
+                    node_current += symbols[comp_name]
                 else:
-                    node_current -= symbols[name]
+                    node_current -= (
+                        symbols[comp_name]
+                        if comp_name in symbols
+                        else sp.symbols(comp_name)
+                    )
 
         if node_current != 0:
             kcl_equations[node] = sp.Eq(node_current, 0)
