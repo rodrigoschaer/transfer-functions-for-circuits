419a9858634943cda0a7e2465e0b706b0c09654d
Rodrigo F. Schaer <rodrigo.fscs@gmail.com>
2024-08-06 22:55:15 -0300

    refactor: moves all logic to app entry point, as it will be easier to fix features [will be refactored later]

--- python/steps/calculate_tf_from_kcl.py
+++
@@ -1,8 +0,0 @@
-import sympy as sp
-
-
-def calculate_tf_from_kcl(kcl_equations, input, output, node_symbols):
-    eq_list = list(kcl_equations.values())
-    solutions = sp.solve(eq_list, node_symbols)
-    transfer_function = sp.simplify(solutions[output] / solutions[input])
-    return transfer_function
